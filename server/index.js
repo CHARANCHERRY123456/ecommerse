58bd7a6e233ca5a9e4d4b7cda251bbc1032cf894
C V CHARAN <cherryiiit1234@gmail.com>
2025-09-06 17:50:58 +0530

    feat : add axiosClient

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import cors from 'cors'
+import morgan from 'morgan';
 import dotenv from 'dotenv';
-import router from './router/index.js';
 dotenv.config();
+
+import router from './router/index.js';
+
 import  connectDb from './db.js'
 connectDb()
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"))
 
 app.use(cors('*'));
 app.use("/api", router);
